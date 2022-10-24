50bcfce8512d3b698bf598b8541b9ac7e1afd1e5
Pratyush Singh <90026952+PratyushSingh07@users.noreply.github.com>
2022-10-24 08:48:05 +0530

    StormCI: Add a Custom Progress bar while fetching the json
    - Fixes [1]
    
    [1] :- https://github.com/stormbreaker-project/StormCI/issues/2

--- app/src/main/java/dev/danascape/stormci/ui/DevicesFragment.kt
+++ app/src/main/java/dev/danascape/stormci/ui/DevicesFragment.kt
@@ -1,12 +1,19 @@
 package dev.danascape.stormci.ui
 
 import android.annotation.SuppressLint
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.FoldingCube
+import com.github.ybq.android.spinkit.style.WanderingCubes
+import com.github.ybq.android.spinkit.style.Wave
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import dev.danascape.stormci.R
 import dev.danascape.stormci.adaptor.DevicesListAdaptor
 import dev.danascape.stormci.api.APIClient
@@ -25,6 +32,8 @@ class DevicesFragment : Fragment(R.layout.fragment_devices) {
 
     private lateinit var recyclerView: RecyclerView
 
+    private  var progressBar: ProgressBar? =null
+    private  var doubleBounce: Sprite=WanderingCubes()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -35,15 +44,19 @@ class DevicesFragment : Fragment(R.layout.fragment_devices) {
         recyclerView.adapter = mAdapter
 
         mApiService = APIClient.client.create(DevicesService::class.java)
+        progressBar?.setIndeterminateDrawable(doubleBounce)
+        progressBar?.visibility=View.VISIBLE
         fetchDevicesList()
     }
 
     private fun fetchDevicesList() {
         val call = mApiService!!.fetchDevices()
+
         call.enqueue(object : Callback<DevicesList> {
 
             @SuppressLint("NotifyDataSetChanged")
             override fun onResponse(call: Call<DevicesList>, response: Response<DevicesList>) {
+                progressBar?.visibility=View.GONE
                 Log.d("StormCI", "Total Devices Fetched: " + response.body()!!.items!!.size)
                 val questions = response.body()
                 if (questions != null) {
