0674ff7ef2f675c428625bc3447f1719b281ea4c
danascape <danascape@gmail.com>
2022-11-06 20:02:05 +0530

    StormCI: Add bare maintainer layout

--- app/src/main/java/dev/danascape/stormci/ui/team/CoreTeamFragment.kt
+++ app/src/main/java/dev/danascape/stormci/ui/team/CoreTeamFragment.kt
@@ -3,10 +3,8 @@ package dev.danascape.stormci.ui.team
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.danascape.stormci.R
