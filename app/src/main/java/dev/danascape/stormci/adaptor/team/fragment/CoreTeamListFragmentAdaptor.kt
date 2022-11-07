153ae772feb98a43482d155557b65cf19fcb401a
danascape <danascape@gmail.com>
2022-11-07 15:16:50 +0530

    StormCI: Add github and linkedin layouts

--- app/src/main/java/dev/danascape/stormci/adaptor/team/fragment/CoreTeamListFragmentAdaptor.kt
+++ app/src/main/java/dev/danascape/stormci/adaptor/team/fragment/CoreTeamListFragmentAdaptor.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import dev.danascape.stormci.R
@@ -28,9 +29,18 @@ class CoreTeamListFragmentAdaptor(private val context: Context, private val mCor
                     .load(url)
                     .into(holder.image)
         }
-        holder.positionNumber.text = "Member: ${position + 1}"
         holder.name.text = mCoreTeam[position].name
         holder.title.text = mCoreTeam[position].title
+        holder.github.text = mCoreTeam[position].github
+        holder.linkedin.text = mCoreTeam[position].linkedin
+
+        holder.github.setOnClickListener {
+            Toast.makeText(context, "Clicked on Github Profile Link", Toast.LENGTH_SHORT).show()
+        }
+
+        holder.linkedin.setOnClickListener {
+            Toast.makeText(context, "Clicked on LinkedIn Profile Link", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,9 +48,10 @@ class CoreTeamListFragmentAdaptor(private val context: Context, private val mCor
     }
 
     class TeamViewHolder(val containerView: View) : RecyclerView.ViewHolder(containerView) {
-        val positionNumber: TextView = itemView.findViewById<View>(R.id.tvPositionNumber) as TextView
         val name: TextView = itemView.findViewById<View>(R.id.tvName) as TextView
         val title: TextView = itemView.findViewById<View>(R.id.tvTitle) as TextView
         val image: ImageView = itemView.findViewById<ImageView>(R.id.imgProfile) as ImageView
+        val github: TextView = itemView.findViewById<View>(R.id.tvGithub) as TextView
+        val linkedin: TextView = itemView.findViewById<View>(R.id.tvLinkedin) as TextView
     }
 }
--- app/src/main/java/dev/danascape/stormci/model/team/CoreTeam.kt
+++ app/src/main/java/dev/danascape/stormci/model/team/CoreTeam.kt
@@ -4,4 +4,6 @@ class CoreTeam {
     val name: String? = null
     val image: String? = null
     val title: String? = null
+    val github: String? = null
+    val linkedin: String? = null
 }
